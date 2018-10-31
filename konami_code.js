00212bef66dc193ce835afac2818660797af18dd
milanla <milan.lanlada@gmail.com>
2018-10-31 19:56:16 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+
+  document.body.addEventListener("keydown", function(e) {
+    let key = e.key;
+
+    if (key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Hurrah!");
+        }
+    } else {
+        index = 0;
+    }    
+  })
 }
