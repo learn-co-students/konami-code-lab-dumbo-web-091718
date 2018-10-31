933301a6da7312e7c1f03a8f4a0a8d0e0f4e337d
Brian DiRito <bdirito@gmail.com>
2018-10-31 10:42:08 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0
+
+  document.body.addEventListener("keydown", function(e){
+     const key = e.key
+
+     if (key === codes[index]) {
+       index ++;
+
+       if (index === codes.length){
+         alert("You have 30 lives!")
+       }
+     } else {
+       index = 0
+     }
+  })
+
 }
